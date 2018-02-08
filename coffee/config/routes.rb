6abb77035428b40a196b94d4b24ba78f007d3fca
eugeneKang <eugenekang94@gmail.com>
2018-02-08 13:48:37 +0900

    turbolink 제거 후 routes 테스트

--- coffee/config/routes.rb
+++ coffee/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :orders
+
+  get '/outlines' => 'outlines#index'
   # root
   root 'outlines#index'
 end
